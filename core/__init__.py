0a67f802f52d91bce67efc1303b8d57c542a2d5e
blibilijojo <1271696610@qq.com>
2026-03-22 18:49:51 +0800

    docs(core): 添加核心模块的导出列表和导入语句
    添加__init__.py文件以明确导出核心模块的公共接口，方便其他模块导入使用

---
+++ core/__init__.py
@@ -0,0 +1,39 @@
+# Core module exports
+from .builder import Builder
+from .data_aggregator import DataAggregator
+from .decision_engine import DecisionEngine
+from .dictionary_manager import DictionaryManager
+from .exceptions import *
+from .extractor import Extractor
+from .models import (
+    ExtractionResult, TranslationResult, PackSettings,
+    WorkflowContext, LanguageEntry, NamespaceInfo,
+    DictionaryEntry
+)
+from .orchestrator import Orchestrator
+from .pack_builder import PackBuilder
+from .quest_converter import QuestConverter
+from .term_database import TermDatabase
+from .translator import Translator
+from .workflow import Workflow
+
+__all__ = [
+    'Builder',
+    'DataAggregator',
+    'DecisionEngine',
+    'DictionaryManager',
+    'Extractor',
+    'Orchestrator',
+    'PackBuilder',
+    'QuestConverter',
+    'TermDatabase',
+    'Translator',
+    'Workflow',
+    'ExtractionResult',
+    'TranslationResult',
+    'PackSettings',
+    'WorkflowContext',
+    'LanguageEntry',
+    'NamespaceInfo',
+    'DictionaryEntry'
+]
